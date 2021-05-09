d98de547b09e3e0d8def73cf8667af6bbda4bb8d
tushal012 <tushalkashyap11@gmail.com>
2021-05-09 20:03:16 +0530

    multiple times flipping a coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,11 +2,38 @@
 	
  echo  "welcome to toss"
 
-  Result=$((RANDOM%2))
-	if [[ ${Result} -eq 0 ]];
-	 then
-    		echo coin is  HEADS
-	elif [[ ${Result} -eq 1 ]];
-	 then
-    		echo coin is  TAILS
-	fi
+#  Result=$((RANDOM%2))
+#	if [[ ${Result} -eq 0 ]];
+#	 then
+ #   		echo coin is  HEADS
+#	elif [[ ${Result} -eq 1 ]];
+#	 then
+ #   		echo coin is  TAILS
+#	fi
+
+
+function TossHT(){
+	local heads=$1
+	local Tails=$2
+while [ $count -lt 12 ]
+	toss=$((RANDOM%3))
+do
+	if  [[ $toss -eq 1 ]];
+		 
+		Heads +=1
+		flips +=1
+		echo Heads
+	
+	elif [[ $toss -eq 2 ]];
+	
+		Tails +=1
+		Flips +=1	
+		echo Tails
+	
+done
+}
+heads=0
+tails=0
+count=0
+result="$( TossHT $Heads $Tails )"
+echo $result
