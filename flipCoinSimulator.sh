8f1b6a26c8b60fa112006d8d59958e186161ea74
tushal012 <tushalkashyap11@gmail.com>
2021-05-09 19:31:02 +0530

    toss is heads or tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,3 +2,11 @@
 	
  echo  "welcome to toss"
 
+  Result=$((RANDOM%2))
+	if [[ ${Result} -eq 0 ]];
+	 then
+    		echo coin is  HEADS
+	elif [[ ${Result} -eq 1 ]];
+	 then
+    		echo coin is  TAILS
+	fi
